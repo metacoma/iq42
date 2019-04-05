7f55f08b83861dd2f4babeeb54164cb95e6f6e85
Sergey Ryabin <metacoma@github.com>
2019-04-05 23:57:48 +0400

    icon

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    //w.setWindowIcon(QIcon(":../../Downloads/iq42_icon.png"));
+    w.setWindowIcon(QIcon(":/images/iq42_icon32x32.png"));
     w.show();
 
     return a.exec();
