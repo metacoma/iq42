ae11a9e5c3290bb56b90e556a4ffc60aebc7dc96
Sergey Ryabin <metacoma@github.com>
2019-04-06 00:10:03 +0400

    added QSystemTrayIcon

--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@
 
 int main(int argc, char *argv[])
 {
+
+
     QApplication a(argc, argv);
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(nullptr, QObject::tr("Systray"),
+                              QObject::tr("I couldn't detect any system tray "
+                                          "on this system."));
+        return 1;
+    }
+
+
+
     MainWindow w;
-    //w.setWindowIcon(QIcon(":../../Downloads/iq42_icon.png"));
     w.setWindowIcon(QIcon(":/images/iq42_icon32x32.png"));
     w.show();
 
