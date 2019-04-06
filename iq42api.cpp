ad6c3252dab31c368340af488c368d160111baa7
Sergey Ryabin <metacoma@github.com>
2019-04-06 04:08:55 +0400

    Ping reply handled

--- iq42api.cpp
+++ iq42api.cpp
@@ -1,21 +1,33 @@
 #include "iq42api.h"
 
-IQ42Api::IQ42Api()
+IQ42Api::IQ42Api() : QObject ()
 {
     setBackendURL("http://8aff09151ba7.sn.mynetname.net:18080");
 
 
 }
 
+IQ42Api::~IQ42Api() {
+    qDebug() << "Destroy Api QObject";
+}
+
+void IQ42Api::PingFinished() {
+    qDebug() << "Ping reply finished";
+}
+
 void IQ42Api::Ping() {
     QUrl pingUrl = QUrl(getBackendURL().toString() + "/ping");
-    qnam.get(QNetworkRequest(pingUrl));
+    reply = qnam.get(QNetworkRequest(pingUrl));
+    QObject::connect(reply, &QNetworkReply::finished, this, &IQ42Api::PingFinished);
 }
 
 void IQ42Api::setBackendURL(QString URL) {
     backendURL = QUrl(URL);
 }
 
+
+
 QUrl IQ42Api::getBackendURL() {
     return backendURL;
 }
+
--- iq42api.h
+++ iq42api.h
@@ -1,24 +1,37 @@
 #ifndef IQ42API_H
 #define IQ42API_H
 
+#include <QObject>
 #include <QString>
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QUrl>
+#include <QDebug>
+#include <QNetworkReply>
 
 
-class IQ42Api
+
+class IQ42Api : public QObject
 {
+    Q_OBJECT
+//    Q_INTERFACES(QObject)
+
 public:
     IQ42Api();
+    ~IQ42Api();
     void setBackendURL(QString URL);
     QUrl getBackendURL();
 
     void Ping();
 
+public slots:
+    void PingFinished();
+
+
 private:
     QUrl backendURL;
     QNetworkAccessManager qnam;
+    QNetworkReply *reply;
 };
 
 #endif // IQ42API_H
